812e2f6c2bc53e19d1a1c9caa6caa9dcbe2761e0
Sverre Johann Bjørke <sverrejb@gmail.com>
2017-03-14 10:12:31 +0100

    formatting

--- EA/main.py
+++ EA/main.py
@@ -1,18 +1,15 @@
+import copy
+import subprocess
+import time
 from random import randint
 
-from cards import read_card_pool
+from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
-from deap import algorithms
-import copy
-import time
+
 import config
-
-
-import subprocess
-
-from pprint import pprint
+from cards import read_card_pool
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
@@ -20,7 +17,7 @@ creator.create("Individual", list, fitness=creator.FitnessMax)
 POPSIZE = 10
 DECKSIZE = 40
 NUMBER_OF_GENERATIONS = 20
-MATCHES_PER_OPPONENT="10"
+MATCHES_PER_OPPONENT = "10"
 CARD_POOL = read_card_pool('../AER-POOL-1.txt')
 CARD_POOL_SIZE = len(CARD_POOL)
 CARD_DIRECTORY = config.CARD_DIR
@@ -65,7 +62,7 @@ def mutate_deck(individual):
     return individual,  # MUST RETURN TUPLE
 
 
-def build_cmd(candidate_name, opponent_name,nr_matches):
+def build_cmd(candidate_name, opponent_name, nr_matches):
     return ['java', '-Xmx1024m', '-jar',
             'forge-gui-desktop-1.5.61-SNAPSHOT-jar-with-dependencies.jar', 'sim',
             '-d', candidate_name, opponent_name,
@@ -75,14 +72,14 @@ def build_cmd(candidate_name, opponent_name,nr_matches):
 def evaluate_deck(individual):
     decklist = genome_to_decklist(individual)
     filename = 'candidate.dck'
-    #opponent = 'Merfolk.dck'
+    # opponent = 'Merfolk.dck'
     fitness = 0
-    opponents=["GB-sealed-opponent.dck","UWg-sealed-opponent.dck"]
+    opponents = ["GB-sealed-opponent.dck", "UWg-sealed-opponent.dck"]
     for opponent in opponents:
-        with open(CARD_DIRECTORY+filename, 'w') as file:
+        with open(CARD_DIRECTORY + filename, 'w') as file:
             file.write('[metadata]\nName=candidate\n[Main]\n')
             for card in decklist:
-                file.write(card+'\n')
+                file.write(card + '\n')
 
         cmd = build_cmd(filename, opponent, MATCHES_PER_OPPONENT)
 
@@ -93,7 +90,7 @@ def evaluate_deck(individual):
                 result = line.split(' ')
         p.wait()
         fitness += int(result[3])
-    #print(fitness)
+    # print(fitness)
     return fitness,  # MUST BE TUPLE!
 
 
@@ -105,13 +102,14 @@ def init_population(pcls, ind_init, list_of_decks):
     return pcls(ind_init(c) for c in list_of_decks)
 
 
-def mate_individuals(ind1,ind2):
-    templist=sorted(copy.deepcopy(ind1)+copy.deepcopy(ind2))
-    list_ind1=templist[0::2]
-    list_ind2=templist[1::2]
+def mate_individuals(ind1, ind2):
+    templist = sorted(copy.deepcopy(ind1) + copy.deepcopy(ind2))
+    list_ind1 = templist[0::2]
+    list_ind2 = templist[1::2]
     for i in range(len(list_ind1)):
-        ind1[i], ind2[i]=list_ind1[i], list_ind2[i]
+        ind1[i], ind2[i] = list_ind1[i], list_ind2[i]
     return ind1, ind2
+
 
 first_gen_decks = generate_first_generation_decks(CARD_POOL)
 
@@ -120,25 +118,21 @@ toolbox = base.Toolbox()
 toolbox.register("individual_guess", init_individual, creator.Individual)
 toolbox.register("card_population", init_population, list, toolbox.individual_guess, first_gen_decks)
 
-
 toolbox.register("evaluate", evaluate_deck)
 toolbox.register("mate", mate_individuals)
 toolbox.register("mutate", mutate_deck)
 toolbox.register("select", tools.selTournament, tournsize=3)
-
-
-
 
 # TODO: VELG BREEDING OG MUTASJONSSTRATEGI
 
 # TODO: VURDER fitnessfunksjon til å returnere skade på motstander, evt antall hits på motstander
 
 population = toolbox.card_population()
-tid=time.time()
+tid = time.time()
 for gen in range(NUMBER_OF_GENERATIONS):
     offspring = algorithms.varAnd(population, toolbox, cxpb=0.1, mutpb=0.02)
     fits = toolbox.map(toolbox.evaluate, offspring)
-    print (list(fits))
+    print(list(fits))
     for fit, ind in zip(fits, offspring):
         ind.fitness.values = fit
     population = toolbox.select(offspring, k=len(population))
@@ -147,4 +141,4 @@ top10 = tools.selBest(population, k=10)
 for individual in top10:
     print(individual)
 
-print (time.time()-tid)
+print(time.time() - tid)
