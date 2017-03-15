d6f324496df6ae2d5d9f46f1040a66fbdba65a3c
Sverre Johann Bjørke <sverrejb@gmail.com>
2017-03-15 17:29:15 +0100

    refactoring

--- EA/main.py
+++ EA/main.py
@@ -13,7 +13,6 @@ from deap import tools
 import config
 from cards import read_card_pool
 
-import matplotlib.pyplot as plt
 
 from scoop import futures
 
@@ -28,88 +27,80 @@ CARD_DIRECTORY = config.CARD_DIR
 FORGE_PATH = config.FORGE_DIR
 DECKLIST_HEADER = '[metadata]\nName=candidate\n[Main]\n'
 
-
-def main():
-
-    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-    creator.create("Individual", list, fitness=creator.FitnessMax)
-
-
-
-    def genome_to_decklist(individual):
-        deck_list = []
-        for c in individual:
-            deck_list.append(CARD_POOL[c][0])
-        return deck_list
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+def genome_to_decklist(individual):
+    deck_list = []
+    for c in individual:
+        deck_list.append(CARD_POOL[c][0])
+    return deck_list
 
 
-    def generate_individual(card_pool):
-        individual = []
-        card_pool_size = len(card_pool)
-        while len(individual) < 40:
-            genome = randint(0, card_pool_size - 1)
-            number_of_genome_present = individual.count(genome)
-            if number_of_genome_present < card_pool[genome][1]:
-                individual.append(genome)
-        return individual
+def generate_individual(card_pool):
+    individual = []
+    card_pool_size = len(card_pool)
+    while len(individual) < 40:
+        genome = randint(0, card_pool_size - 1)
+        number_of_genome_present = individual.count(genome)
+        if number_of_genome_present < card_pool[genome][1]:
+            individual.append(genome)
+    return individual
 
 
-    def generate_first_generation_decks(card_pool):
-        population = []
-        for i in range(POPSIZE):
-            population.append(generate_individual(card_pool))
-        return population
+def generate_first_generation_decks(card_pool):
+    population = []
+    for i in range(POPSIZE):
+        population.append(generate_individual(card_pool))
+    return population
 
 
-    def mutate_deck(individual):
-        size = len(individual)
-        mutation_site = randint(0, size - 1)
-        mutated = False
-        while not mutated:
-            new_gene = randint(0, CARD_POOL_SIZE - 1)
-            gene_limit = CARD_POOL[new_gene][1]
-            if individual.count(new_gene) < gene_limit:
-                individual[mutation_site] = new_gene
-                mutated = True
-        return individual,  # MUST RETURN TUPLE
+def mutate_deck(individual):
+    size = len(individual)
+    mutation_site = randint(0, size - 1)
+    mutated = False
+    while not mutated:
+        new_gene = randint(0, CARD_POOL_SIZE - 1)
+        gene_limit = CARD_POOL[new_gene][1]
+        if individual.count(new_gene) < gene_limit:
+            individual[mutation_site] = new_gene
+            mutated = True
+    return individual,  # MUST RETURN TUPLE
 
 
-    def build_cmd(candidate_name, opponent_name, nr_matches):
-        return ['java', '-Xmx1024m', '-jar',
-                'forge-gui-desktop-1.5.61-SNAPSHOT-jar-with-dependencies.jar', 'sim',
-                '-d', candidate_name, opponent_name,
-                '-n', nr_matches, '-f', 'sealed']
+def build_cmd(candidate_name, opponent_name, nr_matches):
+    return ['java', '-Xmx1024m', '-jar',
+            'forge-gui-desktop-1.5.61-SNAPSHOT-jar-with-dependencies.jar', 'sim',
+            '-d', candidate_name, opponent_name,
+            '-n', nr_matches, '-f', 'sealed']
 
 
-    def evaluate_deck_by_damage(individual):
-        decklist = genome_to_decklist(individual)
-        filename = 'candidate.dck'
-        total_damage = 0
-        wins = 0
-        opponents = ["GB-sealed-opponent.dck", "UWg-sealed-opponent.dck"]
-        for opponent in opponents:
-            with open(CARD_DIRECTORY + filename, 'w') as file:
-                file.write(DECKLIST_HEADER)
-                for card in decklist:
-                    file.write(card + '\n')
-
-            cmd = build_cmd(filename, opponent, MATCHES_PER_OPPONENT)
-            p = subprocess.Popen(cmd, cwd=FORGE_PATH, stdout=subprocess.PIPE)
-            for line in p.stdout:
-                line = line.decode("utf-8").strip()
-                if 'combat damage to Ai(2' in line:
-                    hit_event = line.split(' ')
-                    #print(hit_event) #For debugging
-                    damage_index = hit_event.index('deals') + 1
-                    damage = int(hit_event[damage_index])
-                    total_damage += damage
-                if 'Match result' in line:
-                    result = line.split(' ')
-                    wins += int(result[3])
-
-            p.wait()
-            fitness = wins * total_damage   #(wins/float(MATCHES_PER_OPPONENT*len(opponents)))*damage
-        return fitness,  # MUST BE TUPLE!
+def evaluate_deck_by_damage(individual):
+    decklist = genome_to_decklist(individual)
+    filename = 'candidate.dck'
+    total_damage = 0
+    wins = 0
+    opponents = ["GB-sealed-opponent.dck", "UWg-sealed-opponent.dck"]
+    for opponent in opponents:
+        with open(CARD_DIRECTORY + filename, 'w') as file:
+            file.write(DECKLIST_HEADER)
+            for card in decklist:
+                file.write(card + '\n')
+        cmd = build_cmd(filename, opponent, MATCHES_PER_OPPONENT)
+        p = subprocess.Popen(cmd, cwd=FORGE_PATH, stdout=subprocess.PIPE)
+        for line in p.stdout:
+            line = line.decode("utf-8").strip()
+            if 'combat damage to Ai(2' in line:
+                hit_event = line.split(' ')
+                #print(hit_event) #For debugging
+                damage_index = hit_event.index('deals') + 1
+                damage = int(hit_event[damage_index])
+                total_damage += damage
+            if 'Match result' in line:
+                result = line.split(' ')
+                wins += int(result[3])
+        p.wait()
+        fitness = wins * total_damage   #(wins/float(MATCHES_PER_OPPONENT*len(opponents)))*damage
+    return fitness,  # MUST BE TUPLE!
 
 
     # def evaluate_deck(individual):
@@ -137,21 +128,26 @@ def main():
     #     return fitness,  # MUST BE TUPLE!
 
 
-    def init_individual(icls, content):
-        return icls(content)
+def init_individual(icls, content):
+    return icls(content)
 
 
-    def init_population(pcls, ind_init, list_of_decks):
-        return pcls(ind_init(c) for c in list_of_decks)
+def init_population(pcls, ind_init, list_of_decks):
+    return pcls(ind_init(c) for c in list_of_decks)
 
 
-    def mate_individuals(ind1, ind2):
-        templist = sorted(copy.deepcopy(ind1) + copy.deepcopy(ind2))
-        list_ind1 = templist[0::2]
-        list_ind2 = templist[1::2]
-        for i in range(len(list_ind1)):
-            ind1[i], ind2[i] = list_ind1[i], list_ind2[i]
-        return ind1, ind2
+def mate_individuals(ind1, ind2):
+    templist = sorted(copy.deepcopy(ind1) + copy.deepcopy(ind2))
+    list_ind1 = templist[0::2]
+    list_ind2 = templist[1::2]
+    for i in range(len(list_ind1)):
+        ind1[i], ind2[i] = list_ind1[i], list_ind2[i]
+    return ind1, ind2
+
+
+def main():
+
+
 
 
     first_gen_decks = generate_first_generation_decks(CARD_POOL)
@@ -198,11 +194,6 @@ def main():
     print(fitness_list)
     print(type(fitness_list[0]))
 
-    image_name = str(datetime.datetime.now()) + '.png'
-
-    # plt.plot([x[0] for x in fitness_list])
-    # plt.ylabel('some numbers')
-    # plt.savefig(image_name)
 
 if __name__ == '__main__':
     main()
