22ae26d3a37b1495b2fa177eb2fed4a877e96254
Sverre Johann Bjørke <sverrejb@gmail.com>
2017-03-20 15:44:51 +0100

    fix

--- EA/main.py
+++ EA/main.py
@@ -8,7 +8,7 @@ from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
-#from scoop import futures
+from scoop import futures
 
 import constants as ct
 from decks import genome_to_decklist, write_decklist
@@ -91,7 +91,7 @@ def main():
 
     for gen in range(ct.NUMBER_OF_GENERATIONS):
         offspring = algorithms.varAnd(population, toolbox, cxpb=ct.CROSSOVER_RATE, mutpb=ct.MUTATION_RATE)
-        fits = list(map(toolbox.evaluate, offspring))  #list(futures.map(toolbox.evaluate, offspring))
+        fits = list(futures.map(toolbox.evaluate, offspring))
         print(fits)
         for fit, ind in zip(fits, offspring):
             print(fit)
