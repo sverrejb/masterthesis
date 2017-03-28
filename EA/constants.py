64aa10b17a63a2a49e2c43dc27582bbbe3b237b0
Sverre Johann Bjørke <sverrejb@gmail.com>
2017-03-28 21:34:09 +0200

    ready for experiment

--- EA/constants.py
+++ EA/constants.py
@@ -5,11 +5,11 @@ from read_json import read_cards_json
 
 POPSIZE = 10
 DECKSIZE = 40
-CROSSOVER_RATE = 0.5
+CROSSOVER_RATE = 0.7
 MUTATION_RATE = 0.2
 NUMBER_OF_GENERATIONS = 200
 MATCHES_PER_OPPONENT = '50'  # must be string!
-CARD_POOL = read_card_pool('../AER-POOL-1.txt')
+CARD_POOL = read_card_pool('../AER-pool-2.txt')
 CARD_POOL_SIZE = len(CARD_POOL)
 CARD_DIRECTORY = config.CARD_DIR
 FORGE_PATH = config.FORGE_DIR
