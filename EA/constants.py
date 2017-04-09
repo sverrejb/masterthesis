d0a8a1544ec726c1cff9ff8b7bbab227a439c4c3
Sverre Johann Bjørke <sverrejb@gmail.com>
2017-04-09 18:07:15 +0200

    short, single opponent experiment

--- EA/constants.py
+++ EA/constants.py
@@ -6,7 +6,7 @@ from read_json import read_cards_json
 POPSIZE = 10
 DECKSIZE = 40
 CROSSOVER_RATE = 0.5
-MUTATION_RATE = 0.2
+MUTATION_RATE = 0.3
 NUMBER_OF_GENERATIONS = 200
 MATCHES_PER_OPPONENT = '50'  # must be string!
 CARD_POOL = read_card_pool('../AER-pool-2.txt')
@@ -16,7 +16,8 @@ FORGE_PATH = config.FORGE_DIR
 DECKLIST_HEADER = '[metadata]\nName=candidate\n[Main]\n'
 # OPPONENTS = ["GB-sealed-opponent.dck", "UWg-sealed-opponent.dck", "RG-sealed-opponent1.dck",
 #              "BGw-sealed-opponent.dck", "UR-sealed-opponent.dck", "RW-sealed-opponent.dck"]
-OPPONENTS = ["GB-sealed-opponent.dck", "UWg-sealed-opponent.dck", "BGw-sealed-opponent.dck"]
+# OPPONENTS = ["GB-sealed-opponent.dck", "UWg-sealed-opponent.dck", "BGw-sealed-opponent.dck"]
+OPPONENTS = ["RW-sealed-opponent.dck"]
 
 EXPERIMENT_TIMESTAMP = datetime.datetime.now().strftime("%d%m%H%M")
 EXPERIMENT_FOLDER = "results/" + EXPERIMENT_TIMESTAMP
